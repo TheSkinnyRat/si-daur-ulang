22162e9359f1ee882d775db5eed70820fbab0407
TheSkinnyRat <purwa.sabrang@gmail.com>
2023-07-20 21:36:44 +0700

    🐛 Fix headers bug in development

--- pages/api/staff/recycles/[id].ts
+++ pages/api/staff/recycles/[id].ts
@@ -15,7 +15,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const user = await roleCheck(res.getHeader('x-jwt-payload') as string, 'staff', res);
+  const user = await roleCheck(res.getHeader('x-jwt-payload') as string || req.headers['x-jwt-payload'] as string, 'staff', res);
   if (!user) return undefined;
   if (req.method === 'GET') {
     try {
